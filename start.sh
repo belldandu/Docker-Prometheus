7bcacb859731f13380f725a1580a54cac52e7679
Belldandu <michaelfox648@gmail.com>
2017-04-01 23:14:25 -0400

    Update start.sh
    There we go

--- start.sh
+++ start.sh
@@ -10,28 +10,31 @@ if [ "$(pwd)" -ne "/home/container" ]; then
 fi
 
 # Download and compile PSPNET_ADHOCCTL_SERVER, if it is missing
-if [ ! -f "/home/container/aemu/pspnet_adhocctl_server/pspnet_adhocctl_server" ]; then
-	if [ -d "/home/container/aemu/" ]; then
-		# remove existing files to prevent git clashes
-		echo "Removing Existing files to prevent git clashes..."
-		echo ":/home/container$ rm -rf aemu"
-		rm -rf aemu
-	fi
-
-	# clone the aemu repository on github
-	echo "Cloning the aemu repository on github..."
-	echo ":/home/container$ git clone https://github.com/MrColdbird/aemu.git"
-	git clone https://github.com/MrColdbird/aemu.git
-
-	# Compile the PSPNET_ADHOCCTL_SERVER
-	echo "Compiling the PSPNET_ADHOCCTL_SERVER in the cloned repository"
+if [ ! -f "/home/container/pspnet_adhocctl_server" ]; then
+	# Grab the aemu repository from github
+	echo "Grabbing the aemu repository from github..."
+	echo ":/home/container$ curl -Lo https://github.com/belldandu/aemu/archive/v1.0.tar.gz"
+	curl -Lo https://github.com/belldandu/aemu/archive/v1.0.tar.gz
+	
+	# Extract the aemu repo
+	echo "Extracting the aemu repository..."
+	echo ":/home/container$ tar -xjvf v1.0.tar.gz"
+	tar -xjvf v1.0.tar.gz
+	
+	# Move the necessary files out of the aemu repo
+	echo "Moving PSPNET_ADHOCCTL_SERVER out of the aemu repository..."
 	echo ":/home/container$ mv /home/container/aemu/pspnet_adhocctl_server/* /home/container"
 	mv /home/container/aemu/pspnet_adhocctl_server/* /home/container
+	
+	# Clean up the remainder files we don't need
+	echo "Cleaning up the remainder of the files we don't need..."
 	echo ":/home/container$ rm -rf aemu"
 	rm -rf aemu
+	
+	# Compile the PSPNET_ADHOCCTL_SERVER
+	echo "Compiling the PSPNET_ADHOCCTL_SERVER"
 	echo ":/home/container/$ make"
 	make
-	
 else
 	echo "Dependencies in place, to re-download this PSP Adhoc server please delete the aemu directory"
 fi
